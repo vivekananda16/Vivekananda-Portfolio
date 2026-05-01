612ae43bd4d4accbfa9b815a2ed885ab68fb50f6
vivekananda16 <vivekanandamohanty7@gmail.com>
2026-05-01 14:58:12 +0530

    fix: remove unused imports to pass CI build

--- src/components/QALab.jsx
+++ src/components/QALab.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { FaBug, FaRobot, FaServer, FaDatabase, FaCodeBranch } from 'react-icons/fa';
 
 const QALab = () => {
